ea3de11868e67e8b6896515295c568d19a6eb76c
akshaymugale01 <139070784+akshaymugale01@users.noreply.github.com>
2024-02-28 01:53:20 +0530

    CRUD application generated

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const express = require('express')
+
 const app = express();
 
 const config = require('dotenv').config();
 const port = process.env.PORT || 5056 ;
 const jsonData = require('./jsonData');
 
+const Product = require('./models/productModels');
+const mongoose = require('mongoose');
+const mongo_uri= process.env.MONGO_URL;
+const cors = require('cors')
+
+app.use(middleWare)
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: false}))
+
 function middleWare(req, res, next){
-    console.log('Middleware Started')
+    console.log('Middleware Running..')
     next()
 }
 
@@ -22,10 +33,70 @@ app.get('/api/data', (req ,res)=>{
     res.json(jsonData);
 })
 
-app.listen(port, (error)=>{
-    if (error) {
-        console.log("error occured....")
-    } else {
-        console.log(`Server started listenig to port: ${port}`)
+const connectToMongo = async() =>{
+    try {
+        await mongoose.connect(mongo_uri).then(()=>{
+            console.log("DB Connected Sucessfuly")
+            app.listen(port, ()=>{
+                console.log(`Server Listening To Port: ${port}`);
+            });
+        });
+    } catch (error) {
+        console.log("Error connecting to server")
+    }
+}
+
+connectToMongo();
+
+// app.listen(port, (error)=>{
+//     if (error) {
+//         console.log("error occured....")
+//     } else {
+//         console.log(`Server started listenig to port: ${port}`)
+//     }
+// })
+
+
+//Create A Product route
+app.post("/products", async(req, res)=>{
+    try {
+        const product = await Product.create(req.body);
+        res.status(200).json(product);
+    } catch (error) { 
+        console.log(error.message);
+        res.status(500).json({message : error.message});
+    }
+});
+
+app.get('/products', async(req, res)=>{
+    try {
+        const products = await Product.find({})
+        res.status(200).json(products)
+    } catch (error) {
+        console.log("Error Accesing Products")
+        res.status(500).json({message: error.message})
     }
 })
+
+app.delete('/products/:id', async(req, res)=>{
+    try {
+        const {id} = req.params;
+        const product = await Product.findByIdAndDelete(id)
+        if(!product){
+            return status(404).json({message: `Cant Find product with id: ${id}`})
+        }else{
+            res.status(200).json(product)
+        }
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+});
+
+app.delete('/products', async(req, res)=>{
+    try {
+          await Product.deleteMany({});
+          res.status(200).json({message: "All products deletted sucessfully"})
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+});
